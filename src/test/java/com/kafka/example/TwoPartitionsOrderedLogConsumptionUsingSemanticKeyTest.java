f0acaaa1f3a7c4c8834fb7008e34e571dd7f2030
Francesco Losciale <francesco.losciale@thoughtworks.com>
2020-01-11 20:13:00 +0000

    make consumer and producer configuration explicit

--- src/test/java/com/kafka/example/TwoPartitionsOrderedLogConsumptionUsingSemanticKeyTest.java
+++ src/test/java/com/kafka/example/TwoPartitionsOrderedLogConsumptionUsingSemanticKeyTest.java
@@ -3,7 +3,12 @@ package com.kafka.example;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.IntegerDeserializer;
+import org.apache.kafka.common.serialization.IntegerSerializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,9 +16,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.kafka.test.EmbeddedKafkaBroker;
 import org.springframework.kafka.test.context.EmbeddedKafka;
-import org.springframework.kafka.test.utils.KafkaTestUtils;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -21,6 +26,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import static java.lang.Boolean.FALSE;
 import static java.util.Collections.singleton;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -58,7 +64,7 @@ public class TwoPartitionsOrderedLogConsumptionUsingSemanticKeyTest extends Abst
     }
 
     private List<String> consumeRecordValues(List<ConsumerRecord<String, String>> consumerRecordList) {
-        ConsumerRecords<String, String> consumerRecords = KafkaTestUtils.getRecords(this.consumer);
+        ConsumerRecords<String, String> consumerRecords = consumer.poll(Duration.ofSeconds(5));
         consumerRecords.iterator().forEachRemaining(consumerRecordList::add);
         return consumerRecordList.stream().map(
                 record -> record.value()).collect(Collectors.toList()
@@ -81,19 +87,29 @@ public class TwoPartitionsOrderedLogConsumptionUsingSemanticKeyTest extends Abst
 
     @Override
     Map<String, Object> createProducerConfig() {
-        HashMap<String, Object> configs = new HashMap<>(
-                KafkaTestUtils.producerProps(embeddedKafkaBroker_TwoPartitions)
-        );
-        return configs;
+        Map<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, embeddedKafkaBroker_TwoPartitions.getBrokersAsString());
+        props.put(ProducerConfig.RETRIES_CONFIG, 0);
+        props.put(ProducerConfig.BATCH_SIZE_CONFIG, "16384");
+        props.put(ProducerConfig.LINGER_MS_CONFIG, 1);
+        props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, "33554432");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, IntegerSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        return props;
     }
 
     @Override
     Map<String, Object> createConsumerConfig() {
-        Map<String, Object> configs = new HashMap<>(
-                KafkaTestUtils.consumerProps("consumerGroupName"+getClass(), "false", embeddedKafkaBroker_TwoPartitions)
-        );
-        configs.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-        return configs;
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, embeddedKafkaBroker_TwoPartitions.getBrokersAsString());
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "consumerGroupName");
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, FALSE);
+        props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "10");
+        props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "60000");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, IntegerDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        return props;
     }
 
 }
